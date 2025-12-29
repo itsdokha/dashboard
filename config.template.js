75dbbb31d51bc90de60f287a8250cc732b669cce
itsdokha <gaibov.dadodzhon@bk.ru>
2025-12-30 02:00:18 +0300

    minor fixes

---
+++ config.template.js
@@ -0,0 +1,18 @@
+/**
+ * Secret configuration - Built from template by GitHub Actions
+ */
+const SECRETS = {
+    github: {
+        token: '{{GITHUB_TOKEN}}',
+    },
+    telegram: {
+        botToken: '{{TELEGRAM_BOT_TOKEN}}',
+        userId: {{TELEGRAM_USER_ID}},
+    },
+    lastfm: {
+        apiKey: '{{LASTFM_API_KEY}}',
+    },
+    tmdb: {
+        apiKey: '{{TMDB_API_KEY}}',
+    },
+};
